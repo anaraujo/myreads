42242ed8f295c4ed6d427f53c31df962fcdf853b
Ana Laura de Araujo <anaraujo0847@gmail.com>
2024-12-24 17:07:49 -0400

    chore: create book component props

--- starter/src/Book.js
+++ starter/src/Book.js
@@ -1,14 +1,14 @@
-const Book = () => {
+const Book = ({ cover, title, author }) => {
+  const { width, height, url } = cover;
   return (
     <div className="book">
       <div className="book-top">
         <div
           className="book-cover"
           style={{
-            width: 128,
-            height: 193,
-            backgroundImage:
-              'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+            width,
+            height,
+            backgroundImage: `url("${url}")`,
           }}
         ></div>
         <div className="book-shelf-changer">
@@ -23,8 +23,8 @@ const Book = () => {
           </select>
         </div>
       </div>
-      <div className="book-title">To Kill a Mockingbird</div>
-      <div className="book-authors">Harper Lee</div>
+      <div className="book-title">{title}</div>
+      <div className="book-authors">{author}</div>
     </div>
   );
 };
